c5e1647f7a32c50f383f461fbd77d4b36b94966d
Ashish <sahashish100@gmail.com>
2023-07-19 13:13:29 +0530

    Getting current lat, long and address

---
+++ base/src/main/java/com/example/android/whileinuselocation/ForegroundOnlyLocationService.kt
@@ -0,0 +1,312 @@
+package com.example.android.whileinuselocation
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.io.IOException
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "ForegroundOnlyLocationS"
+
+class ForegroundOnlyLocationService : Service() {
+    private var configurationChange = false
+
+
+    private var serviceRunningInForeground = false
+
+    private val localBinder = LocalBinder()
+
+    private lateinit var notificationManager: NotificationManager
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private lateinit var locationRequest: LocationRequest
+
+    private lateinit var locationCallback: LocationCallback
+
+    private var currentLocation: Location? = null
+
+    override fun onCreate() {
+        Log.d(TAG, "onCreate()")
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest.create().apply {
+            interval = TimeUnit.SECONDS.toMillis(60)
+            fastestInterval = TimeUnit.SECONDS.toMillis(30)
+            maxWaitTime = TimeUnit.MINUTES.toMillis(2)
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            smallestDisplacement = 100f
+        }
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                currentLocation = locationResult.lastLocation
+
+                val latitude = currentLocation?.latitude
+                val longitude = currentLocation?.longitude
+
+               if(latitude != null && longitude != null) {
+                    val address = getAddressFromLocation(latitude, longitude)
+
+                    val intent = Intent(ACTION_FOREGROUND_ONLY_LOCATION_BROADCAST)
+                    intent.putExtra(EXTRA_LOCATION, currentLocation)
+                    intent.putExtra(EXTRA_ADDRESS, address)
+                    LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
+                    if (serviceRunningInForeground) {
+                        notificationManager.notify(
+                            NOTIFICATION_ID,
+                            generateNotification(currentLocation)
+                        )
+                    }
+                }
+            }
+        }
+
+    }
+    // get Address
+
+    private fun getAddressFromLocation(latitude: Double,longitude: Double): String{
+        val geocoder = Geocoder(this, Locale.getDefault())
+        var addressText = ""
+        Log.d(TAG, "getAddressFromLocation: ")
+
+
+        try {
+
+            val addresses: List<Address>? = geocoder.getFromLocation(latitude, longitude, 1)
+
+            if (addresses != null && addresses.isNotEmpty()) {
+                val address: Address = addresses[0]
+                val addressParts = ArrayList<String>()
+
+                if (address.featureName != null) {
+                    addressParts.add(address.featureName)
+                }
+                if (address.subLocality != null) {
+                    addressParts.add(address.subLocality)
+                }
+                if (address.locality != null) {
+                    addressParts.add(address.locality)
+                }
+                if (address.adminArea != null) {
+                    addressParts.add(address.adminArea)
+                }
+                if (address.countryName != null) {
+                    addressParts.add(address.countryName)
+                }
+                addressText = addressParts.joinToString(separator = " ,")
+            } else{
+                Log.d(TAG, "No address found for the location.")
+            }
+        } catch (e: IOException){
+            Log.d(TAG, "No Address from Geocoder: ${e.message}")
+            e.printStackTrace()
+        }
+        Log.d(TAG, "Address from Geocoder: $addressText")
+        return addressText
+
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d(TAG, "onStartCommand()")
+
+        val cancelLocationTrackingFromNotification =
+            intent.getBooleanExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, false)
+
+        if (cancelLocationTrackingFromNotification) {
+            unsubscribeToLocationUpdates()
+            stopSelf()
+        }
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        Log.d(TAG, "onBind()")
+
+        stopForeground(true)
+        serviceRunningInForeground = false
+        configurationChange = false
+        return localBinder
+    }
+
+    override fun onRebind(intent: Intent) {
+        Log.d(TAG, "onRebind()")
+
+        stopForeground(true)
+        serviceRunningInForeground = false
+        configurationChange = false
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        Log.d(TAG, "onUnbind()")
+
+        if (!configurationChange && SharedPreferenceUtil.getLocationTrackingPref(this)) {
+            Log.d(TAG, "Start foreground service")
+            val notification = generateNotification(currentLocation)
+            startForeground(NOTIFICATION_ID, notification)
+            serviceRunningInForeground = true
+        }
+
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(!isAppRunningInForeground()) {
+            stopForeground(true)
+            stopSelf()
+        }
+        Log.d(TAG, "onDestroy()")
+    }
+
+    private fun isAppRunningInForeground(): Boolean{
+        return true
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        configurationChange = true
+    }
+
+    fun subscribeToLocationUpdates() {
+        Log.d(TAG, "subscribeToLocationUpdates()")
+
+        SharedPreferenceUtil.saveLocationTrackingPref(this, true)
+
+        startService(Intent(applicationContext, ForegroundOnlyLocationService::class.java))
+
+        try {
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.getMainLooper())
+        } catch (unlikely: SecurityException) {
+            SharedPreferenceUtil.saveLocationTrackingPref(this, false)
+            Log.e(TAG, "Lost location permissions. Couldn't remove updates. $unlikely")
+        }
+    }
+
+    fun unsubscribeToLocationUpdates() {
+        Log.d(TAG, "unsubscribeToLocationUpdates()")
+
+        try {
+            val removeTask = fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            removeTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Location Callback removed.")
+                    stopSelf()
+                } else {
+                    Log.d(TAG, "Failed to remove Location Callback.")
+                }
+            }
+            SharedPreferenceUtil.saveLocationTrackingPref(this, false)
+
+        } catch (unlikely: SecurityException) {
+            SharedPreferenceUtil.saveLocationTrackingPref(this, true)
+            Log.e(TAG, "Lost location permissions. Couldn't remove updates. $unlikely")
+        }
+    }
+
+
+    private fun generateNotification(location: Location?): Notification {
+        Log.d(TAG, "generateNotification()")
+
+        val mainNotificationText = location?.toText() ?: getString(R.string.no_location_text)
+        val titleText = getString(R.string.app_name)
+
+        // 1. Create Notification Channel for O+ and beyond devices (26+).
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID, titleText, NotificationManager.IMPORTANCE_DEFAULT)
+
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val bigTextStyle = NotificationCompat.BigTextStyle()
+            .bigText(mainNotificationText)
+            .setBigContentTitle(titleText)
+
+        val launchActivityIntent = Intent(this, MainActivity::class.java)
+
+        val cancelIntent = Intent(this, ForegroundOnlyLocationService::class.java)
+        cancelIntent.putExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, true)
+
+        val servicePendingIntent = PendingIntent.getService(
+            this, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val activityPendingIntent = PendingIntent.getActivity(
+            this, 0, launchActivityIntent,  0)
+
+        val notificationCompatBuilder =
+            NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+
+        return notificationCompatBuilder
+            .setStyle(bigTextStyle)
+            .setContentTitle(titleText)
+            .setContentText(mainNotificationText)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setOngoing(true)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .addAction(
+                R.drawable.ic_launch, getString(R.string.launch_activity),
+                activityPendingIntent
+            )
+            .addAction(
+                R.drawable.ic_cancel,
+                getString(R.string.stop_location_updates_button_text),
+                servicePendingIntent
+            )
+            .build()
+    }
+
+    inner class LocalBinder : Binder() {
+        internal val service: ForegroundOnlyLocationService
+            get() = this@ForegroundOnlyLocationService
+    }
+
+    companion object {
+        private const val TAG = "ForegroundOnlyLocationService"
+
+        private const val PACKAGE_NAME = "com.example.android.whileinuselocation"
+
+        internal const val ACTION_FOREGROUND_ONLY_LOCATION_BROADCAST =
+            "$PACKAGE_NAME.action.FOREGROUND_ONLY_LOCATION_BROADCAST"
+
+        internal const val EXTRA_LOCATION = "$PACKAGE_NAME.extra.LOCATION"
+        internal const val EXTRA_ADDRESS = "$PACKAGE_NAME.extra.ADDRESS"
+
+        private const val EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION =
+            "$PACKAGE_NAME.extra.CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION"
+
+        private const val NOTIFICATION_ID = 12345678
+
+        private const val NOTIFICATION_CHANNEL_ID = "while_in_use_channel_01"
+    }
+}
---
+++ base/src/main/java/com/example/android/whileinuselocation/Utils.kt
@@ -0,0 +1,39 @@
+package com.example.android.whileinuselocation
+
+import android.content.Context
+import android.location.Location
+import androidx.core.content.edit
+
+fun Location?.toText(): String {
+    return if (this != null) {
+        "($latitude, $longitude)"
+    } else {
+        "Unknown location"
+    }
+}
+
+internal object SharedPreferenceUtil {
+
+    const val KEY_FOREGROUND_ENABLED = "tracking_foreground_location"
+
+    /**
+     * Returns true if requesting location updates, otherwise returns false.
+     *
+     * @param context The [Context].
+     */
+    fun getLocationTrackingPref(context: Context): Boolean =
+        context.getSharedPreferences(
+            context.getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+            .getBoolean(KEY_FOREGROUND_ENABLED, false)
+
+    /**
+     * Stores the location updates state in SharedPreferences.
+     * @param requestingLocationUpdates The location updates state.
+     */
+    fun saveLocationTrackingPref(context: Context, requestingLocationUpdates: Boolean) =
+        context.getSharedPreferences(
+            context.getString(R.string.preference_file_key),
+            Context.MODE_PRIVATE).edit {
+            putBoolean(KEY_FOREGROUND_ENABLED, requestingLocationUpdates)
+        }
+}
